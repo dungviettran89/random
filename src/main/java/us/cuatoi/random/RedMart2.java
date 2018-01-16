e9764b2f719d584cd429298bbe8db22045e476a0
dungviettran89 <dungviettran89@gmail.com>
2018-01-16 20:21:14 +0800

    Solving http://geeks.redmart.com/2015/10/26/1000000th-customer-prize-another-programming-challenge/

--- src/main/java/us/cuatoi/random/RedMart2.java
+++ src/main/java/us/cuatoi/random/RedMart2.java
@@ -11,57 +11,59 @@ import java.util.Scanner;
 public class RedMart2 {
     public static void main(String[] args) throws IOException {
         String productUrl = "https://s3-ap-southeast-1.amazonaws.com/geeks.redmart.com/coding-problems/products.csv";
-        int maxVol = 45 * 30 * 35;
+        int maxVol = 45 * 30 * 35 + 1;
         int count = 20000;
         try (Scanner scanner = new Scanner(new URL(productUrl).openStream())) {
             scanner.useDelimiter("\\D");
-            int[] max = new int[maxVol + 1];
-            int[] maxWeight = new int[maxVol + 1];
-            List<HashSet<Integer>> ids = new ArrayList<>();
             int[] id = new int[count];
             int[] price = new int[count];
             int[] vol = new int[count];
             int[] weight = new int[count];
+            int max[][] = new int[count][maxVol];
+            int maxWeight[][] = new int[count][maxVol];
+            int maxSum[][] = new int[count][maxVol];
             for (int i = 0; i < count; i++) {
                 id[i] = scanner.nextInt();
                 price[i] = scanner.nextInt();
                 vol[i] = scanner.nextInt() * scanner.nextInt() * scanner.nextInt();
                 weight[i] = scanner.nextInt();
             }
-            for (int i = 0; i < max.length; i++) {
-                max[i] = 0;
-                maxWeight[i] = 0;
-                ids.add(new HashSet<>());
+            for (int j = 0; j < maxVol; j++) {
+                max[0][j] = 0;
+                maxSum[0][j] = 0;
+                maxWeight[0][j] = 0;
             }
-            for (int i = 0; i < count; i++) {
-                for (int j = vol[i] + 1; j < max.length; j++) {
-                    int checkedVol = j - vol[i];
-                    if (ids.get(checkedVol).contains(id[i])) continue;
-                    int newMax = price[i] + max[checkedVol];
-                    if (newMax < max[j]) continue;
-                    int newWeight = weight[i] + maxWeight[checkedVol];
-                    if (newMax == max[j] && newWeight > maxWeight[j]) continue;
-                    max[j] = newMax;
-                    maxWeight[j] = newWeight;
-                    ids.set(j, new HashSet<>(ids.get(checkedVol)));
-                    ids.get(j).add(id[i]);
+            for (int i = 1; i < count; i++) {
+                for (int j = 0; j < maxVol; j++) {
+                    max[i][j] = max[i - 1][j];
+                    maxWeight[i][j] = maxWeight[i - 1][j];
+                    maxSum[i][j] = maxSum[i - 1][j];
+                    if (vol[i] <= j) continue;
+
+                    int newPrice = max[i - 1][j - vol[i]] + price[i];
+                    if (newPrice < max[i][j]) continue;
+                    int newWeight = maxWeight[i - 1][j - vol[i]] + weight[i];
+                    if (newPrice == max[i][j] && maxWeight[i][j] < newWeight) continue;
+                    max[i][j] = newPrice;
+                    maxWeight[i][j] = newWeight;
+                    maxSum[i][j] = maxSum[i - 1][j - vol[i]] + id[i];
                 }
             }
-            int found = 0;
             int globalMax = 0;
-            int globalWeight = Integer.MAX_VALUE;
-            for (int j = 0; j < max.length; j++) {
-                if (globalMax > max[j]) continue;
-                if (globalMax == max[j] && globalWeight < maxWeight[j]) continue;
-                globalMax = max[j];
-                globalWeight = maxWeight[j];
-                found = j;
+            int globalMaxWeight = Integer.MAX_VALUE;
+            int globalMaxSum = 0;
+            for (int j = 0; j < maxVol; j++) {
+                int curMax = max[count - 1][j];
+                if (globalMax > curMax) continue;
+                int curWeight = maxWeight[count - 1][j];
+                if (globalMax == curMax && globalMaxWeight < curWeight) continue;
+                globalMax = curMax;
+                globalMaxWeight = curWeight;
+                globalMaxSum = maxSum[count - 1][j];
             }
-            System.out.println("found=" + found);
             System.out.println("globalMax=" + globalMax);
-            System.out.println("globalWeight=" + globalWeight);
-            System.out.println("ids=" + ids.get(found));
-            System.out.println("sum=" + ids.get(found).stream().reduce(Integer::sum).orElse(0));
+            System.out.println("globalMaxWeight=" + globalMaxWeight);
+            System.out.println("globalMaxSum=" + globalMaxSum);
         }
     }
 }
